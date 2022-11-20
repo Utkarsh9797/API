1e6cdd87a2cf65373731029acb5d716abe7f51d8
UPyati <UPyati@LT322.citi-us.com>
2022-11-21 00:19:58 +0530

    End to End Post-Put-Get method for API Automation testing

---
+++ APIAutomationFramework/src/Basic.java
@@ -0,0 +1,53 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.Assert;
+
+import static io.restassured.RestAssured.*;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		
+		// Post Address and details
+
+ RestAssured.baseURI = "https://rahulshettyacademy.com";
+ String getDetails = given().log().all().header("Content-Type","application/json").queryParam("Key","qaclick123").body(payloads.AddPlace()).when().post("/maps/api/place/add/json").
+then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+System.out.println(getDetails);
+
+JsonPath js = new JsonPath(getDetails);
+String placeid = js.getString("place_id");
+System.out.println(placeid);
+
+// Update the address
+
+String newaddress="CST, USA";
+
+given().queryParam("Key","qaclick123").header("Content-Type","application/json").
+body("{\r\n"
+		+ "\"place_id\":\""+placeid+"\",\r\n"
+		+ "\"address\":\""+newaddress+"\",\r\n"
+		+ "\"key\":\"qaclick123\"\r\n"
+		+ "}")
+.when().put("/maps/api/place/update/json").then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+
+String getadd = given().queryParam("key","qaclick123").queryParam("place_id",placeid).when().get("/maps/api/place/get/json").then().assertThat().statusCode(200).extract()
+.response().asString();
+
+JsonPath js1 = new JsonPath (getadd);
+String actualaddress = js1.getString("address");
+Assert.assertEquals(newaddress, actualaddress);
+
+
+//System.out.println(actualaddress);
+
+
+	}
+	
+	
+
+}
